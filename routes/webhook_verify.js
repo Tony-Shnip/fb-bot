4a09252bd7a21c611bc07c6b950300a5a536e9cf
ardis_by <shnip12345@gmail.com>
2024-01-22 15:56:41 +0300

    update webhook

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -3,13 +3,24 @@ const processMessage = require('../processes/messages');
 
 module.exports = function(app, chalk){
   app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
-       console.log('webhook verified');
-       res.status(200).send(req.query['hub.challenge']);
-    } else {
-        console.error('verification failed. Token mismatch.');
-        res.sendStatus(403);
-     }
+  
+      // Parse the query params
+      let mode = req.query["hub.mode"];
+      let token = req.query["hub.verify_token"];
+      let challenge = req.query["hub.challenge"];
+
+      // Check if a token and mode is in the query string of the request
+      if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+         // Respond with the challenge token from the request
+         console.log("WEBHOOK_VERIFIED");
+         res.status(200).send(challenge);
+      } else {
+         // Respond with '403 Forbidden' if verify tokens do not match
+         res.sendStatus(403);
+      }
+      }
   });
   
   app.post('/webhook', function(req, res) {
